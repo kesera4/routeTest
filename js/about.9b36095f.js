8b28525543c120d79e78150a860dc29852e57011
kesera4 <91398868+kesera4@users.noreply.github.com>
2023-08-22 14:44:18 +0900

    Add files via upload

---
+++ js/about.9b36095f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkrou_2"]=self["webpackChunkrou_2"]||[]).push([[443],{833:function(n,u,r){r.r(u),r.d(u,{default:function(){return l}});var t=r(3396);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),s=[e];function c(n,u){return(0,t.wg)(),(0,t.iD)("div",a,s)}var o=r(89);const i={},f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.9b36095f.js.map
